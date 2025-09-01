3b559f2ac9d1af30f48c9bbf21f33e8d989f0c49
Aliahmadi77sp <aliahmadi77sp@gmail.com>
2025-09-02 03:22:42 +0330

    second test

--- js/app.js
+++ js/app.js
@@ -8,3 +8,21 @@ let isSeller = confirm  ("Are you a seller?");
 console.log(isSeller);
 console.log(typeof isSeller);
 alert(isSeller);
+
+let Result = +prompt("Enter first number" , "0");
+let SecondResult = +prompt("Enter second number" , "0");
+let Plus = Result + SecondResult;
+let Minus = Result - SecondResult;
+let Operations = Result * SecondResult;
+let Division = Result / SecondResult;
+console.log(Plus);
+console.log(Minus);
+console.log(Operations);
+console.log(Division);
+alert(Plus);
+alert(Minus);
+alert(Operations);
+alert(Division);
+
+
+
