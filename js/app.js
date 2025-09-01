2c3ae965087934dd7f4d1ddca3501c8eea284aca
Aliahmadi77sp <aliahmadi77sp@gmail.com>
2025-09-01 22:23:36 +0330

    first training

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let age = +prompt("Enter your age:", "18");
+console.log(age);
+console.log(typeof age);
+alert(age);
+
+
+let isSeller = confirm  ("Are you a seller?");
+console.log(isSeller);
+console.log(typeof isSeller);
+alert(isSeller);
